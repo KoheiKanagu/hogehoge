19c56435c1c745b8608265ca7f614287de9cc0ac
KoheiKanagu <kk.appledeveloper@gmail.com>
2015-02-01 15:39:30 +0900

    newest_gitignore InitialCommit

---
+++ RubySandbox/newest_gitignore.rb
@@ -0,0 +1,44 @@
+require 'net/http'
+
+LIST_FILE = 'gitignoreList'
+
+puts 'Hello!'
+
+unless File.exist?(LIST_FILE)
+  puts '>> gitignoreList Not Found.'
+  puts '>> Did make new file.'
+
+  temp = <<-EOS
+  # A sample newest_gitignore preference
+  # Ruby.gitignore
+  # Objective-C.gitignore
+  EOS
+
+  File.write(LIST_FILE, temp)
+end
+
+list = File.read(LIST_FILE).split("\n")
+
+gitignore = ''
+
+list.each do |variable|
+  variable.strip!
+  next if variable == ''
+  next if /^#/ =~ variable
+
+  gitignore << "# <<<<<<<< #{variable} >>>>>>>>\n"
+  gitignore << "# <<<<<<<< by newest_gitignore.rb >>>>>>>>\n"
+
+  url = 'https://raw.githubusercontent.com/github/gitignore/master/' + variable
+  html = Net::HTTP.get(URI.parse(url))
+
+  if html == 'Not Found'
+    puts ">> #{variable} Not Found"
+  else
+    gitignore << html + "\n\n"
+    puts '>> Done : ' + variable
+  end
+end
+
+File.write('gitignore_global', gitignore)
+puts 'Bye'
